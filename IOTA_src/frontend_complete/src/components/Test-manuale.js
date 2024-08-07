abce70ce1c96e6e51fb038d1b92092dabf0dfff1
giacomo gori <giacomo.gori3@studio.unibo.it>
2024-08-07 16:10:52 +0200

    tests blockchain AUS - USA + update test program

--- IOTA_src/frontend_complete/src/components/Test-manuale.js
+++ IOTA_src/frontend_complete/src/components/Test-manuale.js
@@ -20,22 +20,26 @@ function Testmanuale({ setMessage }) {
   const onTestHandler = async () => {
     //var certificate_pem = "-----BEGINCERTIFICATE-----MIID0jCCArqgAwIBAgIUPsi4pgBuvNEiI2FiZtDGvrXbJYswDQYJKoZIhvcNAQELBQAwTjELMAkGA1UEBhMCSVQxEDAOBgNVBAcMB0JvbG9nbmExDjAMBgNVBAoMBVVuaWJvMQ8wDQYDVQQLDAZVbGlzc2UxDDAKBgNVBAMMA291dDAeFw0yNDAyMDExNTUwMThaFw0zNDAxMjkxNTUwMThaME4xCzAJBgNVBAYTAklUMRAwDgYDVQQHDAdCb2xvZ25hMQ4wDAYDVQQKDAVVbmlibzEPMA0GA1UECwwGVWxpc3NlMQwwCgYDVQQDDANvdXQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDIi/SL4nJ3NBkNyqXbQucsGAMhuHvBMgrR73Mspk2+Fhrg+DHpJ1Syh0pxayiRhRN+lfNkV7bFxGu60OiXuYgVPARWz8JSOKUPH5C/vofgsUCtMaBkqdSvEGftsEqCs4q2l12cFZWNYR6uAVMdAJD5SZbAVBejo/wYOe6eCPE/ykS2QSoYvFwHLaRXZJvpiJNTwyYOA5g/94ykyA7W2/5ZCsFS7XSJAzZEGWjC7ckk0/e/eNmZmGsXjf2VxvfdcAS8P517KITr2dpjWGf9I+l4Q80yBSY0EGV5pgxdDXayV9ernJ1tVA6fnSzxSH1MCwjvA285vX9fjnxR5FwyjP0VAgMBAAGjgacwgaQwDAYDVR0TAQH/BAIwADALBgNVHQ8EBAMCAvQwHQYDVR0lBBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMEkGA1UdEQRCMECGKXVybjpNYWNCb29rLVByby0yLmxvY2FsOlVsaXNzZTpHRFNfY2xpZW50ghNNYWNCb29rLVByby0yLmxvY2FsMB0GA1UdDgQWBBRdbmFzC+oSR0Hgq4WsDTb9DWeyQTANBgkqhkiG9w0BAQsFAAOCAQEAYBohy0qruYp5Y3oAOzFuwhQLj+jk3N+JVP3FuY8g0gV2VjF2M3n8n7alEEXhIgWh5aGoj1u1Z6iJ5crZ/FW7yBTByTbPdBcaJGmbJzAJL7VFljhhGKESzjhznf+daJRbxwjSBeKi4IoDNdpmsGjoq5PJQ06VpBGVDLQA8wTgHSCpS4Q6z6olmAc8TqjToS3mFwCRXsQh2F8N1OBcER2tHNed6XzhmnFJq6PmD/2pv0XKTRIM8eF2LXiMStNZSKzKlT3IMRfya+O+ecOjjYgAqtAxfST0QLPpiV13xk0rUvJ2H/GaoeJ8EtU9SmzQOsLbTUOUkexD7m4JtQoS8v6ctQ==-----ENDCERTIFICATE-----"
    var expireDate = 2022162618;
+   var stampa = "";
    //console.log(certificate_txt);
-   for(var i = 0; i < number; i++){
+   for(var i = 54; i < number; i++){
       setMessage("Running test n." + i);
-      test_pem_time += i + " - " + Date.now() + " - ";
+      stampa += i + " - " + Date.now() + " - ";
       try {
          await scbackend.methods.addCertificate(certificate_txt,expireDate).send({
            from: web3.eth.defaultAccount,
            gas: '10000000',
        });
-       test_pem_time += "sent "+Date.now() + " - ";
+       stampa += "sent "+Date.now();
        } catch (err) {
          setMessage("ERROR (did you remember to previously load the wallet?)");
-         console.log(err)
+         console.log(err);
        }
        sleep(2000);
-       test_pem_time += "\n";    
+       stampa += "\n";
+       test_pem_time += stampa;
+       console.log(stampa);  
+       stampa="";  
    }
    setMessage("TESTS" + number + " done");
    console.log(test_pem_time);
